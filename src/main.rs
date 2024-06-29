58665a1daa7e6b0c9f3fd08c046f36dfdb973511
rpg59 <vanxellshing24@gmail.com>
2024-06-29 20:59:45 +0300

    start

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
+    net::TcpStream
 };
+
+use http;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -33,7 +36,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut req = httparse::Request::new(&mut headers);
             let data_buffer = &buf[0..n];
 
-            let body_offset= req.parse(data_buffer).unwrap().unwrap();
+            let body_offset = req.parse(data_buffer).unwrap().unwrap();
 
 
             // println!("Read data: {:?}", String::from_utf8_lossy(&buf[0..n]));
@@ -45,53 +48,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("Header {} : {}", header.name, String::from_utf8_lossy(header.value));
             }
 
+            println!("Raw {}", String::from_utf8_lossy(data_buffer));
             println!("Body {}", String::from_utf8_lossy(&buf[body_offset..n]));
+
+            let request = http::Request::builder().uri("127.0.0.1:3000").body(()).unwrap();
+
+            let mut connection = TcpStream::connect("127.0.0.1:3000").await.unwrap();
+            connection.write_all(b"GET / HTTP/1.1\r\nHost: localhost:3000").await.unwrap();
+            // connection.write_all(data_buffer).await.unwrap();
+            let mut b1 = [0; 1024];
+            connection.peek(&mut b1).await.unwrap();
+
+            println!("DATA {}", String::from_utf8_lossy(&b1));
+
+            stream.write_all(&b1);
         }
     });
 
     manager.await.unwrap();
     handler.await.unwrap();
     Ok(())
-
-    // loop {
-    //     let (mut socket, _) = listener.accept().await?;
-    //
-    //     tokio::spawn(async move {
-    //         let mut buf = [0; 1024];
-    //
-    //         loop {
-    //             let n = match socket.read(&mut buf).await {
-    //                 Ok(n) if n == 0 => return,
-    //                 Ok(n) => n,
-    //                 Err(e) => {
-    //                     eprintln!("failed to read from socket; err = {:?}", e);
-    //                     return;
-    //                 }
-    //             };
-    //
-    //             if let Err(e) = socket.write_all(&buf[0..n]).await {
-    //                 eprintln!("failed to write to socket; err = {:?}", e);
-    //                 return;
-    //             }
-    //         }
-    //     });
-    // }
 }
 
 // async fn handler() -> Result<()> {
 //
 // }
 
-
-
-
-async fn get_connection() {
-}
-
-
-async fn send_data() {
-    let connection = get_connection().await;
-
-}
 
 
